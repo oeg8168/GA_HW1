518c089dc3daaf5a9f161a19c4af03b4e4f16500
oeg8168 <oeg8168@gmail.com>
2015-04-09 20:58:49 +0800

    Move important functions into GA_HW1

--- src/GA_HW1.java
+++ src/GA_HW1.java
@@ -11,6 +11,8 @@ public class GA_HW1 {
 	public static double upperY = 1.5;
 	public static double lowerY = -0.5;
 
+	public static int numberOfBits = 10; // this cannot be too large
+
 	/**
 	 * Equation to be solve
 	 * 
@@ -40,6 +42,156 @@ public class GA_HW1 {
 		} else {
 			return true;
 		}
-	} // end of checkBound()
+	} // end of inBound()
+
+	/**
+	 * Check if the input is out of bound, with given bounds
+	 * 
+	 * @param input
+	 * @param upper
+	 *            - upper bound
+	 * @param lower
+	 *            - lower bound
+	 * @return boolean value if the input is out of bound
+	 */
+	public static boolean inBound(double input, double upper, double lower) {
+		if (input <= upper && input >= lower) {
+			return true;
+		} else {
+			return false;
+		}
+	} // end of inBound()
+
+	/**
+	 * Take input decimal and encode to binary, with x/y as dimension
+	 * 
+	 * @param decimal
+	 * @param dimension
+	 *            - x or y
+	 * @return encoded string
+	 */
+	public static String encode(double decimal, String dimension) {
+		double fixed = 0;
+
+		if (dimension.equalsIgnoreCase("X")) {
+			if (inBound(decimal, upperX, lowerX)) {
+				fixed = ((decimal - lowerX) / (upperX - lowerX)) * Math.pow(2, numberOfBits);
+			} else {
+				System.err.println("Input out of bound while encoding!!");
+			}
+		} else if (dimension.equalsIgnoreCase("Y")) {
+			if (inBound(decimal, upperY, lowerY)) {
+				fixed = ((decimal - lowerY) / (upperY - lowerY)) * Math.pow(2, numberOfBits);
+			} else {
+				System.err.println("Input out of bound while encoding!!");
+			}
+		} else {
+			System.err.println("Wrong dimension when encoding!!");
+			return null;
+		}
+
+		String bits = Integer.toBinaryString((int) fixed);
+		bits = String.format("%" + numberOfBits + "s", bits).replace(' ', '0');
+		return bits;
+	} // end of encode()
+
+	/**
+	 * Take input decimal and encode to binary, with 0/1 as dimension
+	 * 
+	 * @param decimal
+	 * @param dimension
+	 *            - 0 or 1
+	 * @return encoded string
+	 */
+	public static String encode(double decimal, int dimension) {
+		double fixed = 0;
+
+		if (dimension == 0) {
+			if (inBound(decimal, upperX, lowerX)) {
+				fixed = ((decimal - lowerX) / (upperX - lowerX)) * Math.pow(2, numberOfBits);
+			} else {
+				System.err.println("Input out of bound while encoding!!");
+			}
+		} else if (dimension == 1) {
+			if (inBound(decimal, upperY, lowerY)) {
+				fixed = ((decimal - lowerY) / (upperY - lowerY)) * Math.pow(2, numberOfBits);
+			} else {
+				System.err.println("Input out of bound while encoding!!");
+			}
+		} else {
+			System.err.println("Wrong dimension when encoding!!");
+			return null;
+		}
+
+		String bits = Integer.toBinaryString((int) fixed);
+		bits = String.format("%" + numberOfBits + "s", bits).replace(' ', '0');
+		return bits;
+	} // end of encode()
+
+	/**
+	 * Take input bit string and convert to decimal value, with x/y as dimension
+	 * 
+	 * @param bits
+	 * @param dimension
+	 *            - x or y
+	 * @return decoded value
+	 */
+	public static Double decode(String bits, String dimension) {
+		int index = Integer.parseInt(bits, 2);
+
+		if (dimension.equalsIgnoreCase("X")) {
+			return index / Math.pow(2, numberOfBits) * (upperX - lowerX) + lowerX;
+		} else if (dimension.equalsIgnoreCase("Y")) {
+			return index / Math.pow(2, numberOfBits) * (upperY - lowerY) + lowerY;
+		} else {
+			System.err.println("Wrong dimension when decoding!!");
+			return null;
+		}
+
+	} // end of decode()
+
+	/**
+	 * Take input bit string and convert to decimal value, with 0/1 as dimension
+	 * 
+	 * @param bits
+	 * @param dimension
+	 *            - 0 or 1
+	 * @return decoded value
+	 */
+	public static Double decode(String bits, int dimension) {
+		int index = Integer.parseInt(bits, 2);
+
+		if (dimension == 0) {
+			return index / Math.pow(2, numberOfBits) * (upperX - lowerX) + lowerX;
+		} else if (dimension == 1) {
+			return index / Math.pow(2, numberOfBits) * (upperY - lowerY) + lowerY;
+		} else {
+			System.err.println("Wrong dimension when decoding!!");
+			return null;
+		}
+
+	} // end of decode()
+
+	/**
+	 * Reverse bit at given position
+	 * 
+	 * @param bits
+	 * @param index
+	 * @return reversed bit string
+	 */
+	public static String reverseBit(String bits, int index) {
+
+		char[] temp = bits.toCharArray();
+		if (temp[index] == '0') {
+			temp[index] = '1';
+		} else if (temp[index] == '1') {
+			temp[index] = '0';
+		} else {
+			System.err.println("Reverse bits error!");
+			return null;
+		}
+
+		return new String(temp);
+	} // end of reverseBit()
 
 } // end of class GA_HW1
