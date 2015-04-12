c5d7146a0c33059b8e3641d5109c4a949e370cd8
oeg8168 <oeg8168@gmail.com>
2015-04-12 17:17:40 +0800

    Crossover, mutate, keepBest

--- src/ClassicalGA.java
+++ src/ClassicalGA.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.TreeSet;
+import java.util.Comparator;
 
 /**
  * Data structure of an individual
@@ -78,36 +76,60 @@ class Individual {
 
 public class ClassicalGA {
 
-	public static HashSet<Individual> populations;
-	public static ArrayList<HashSet<Individual>> pathList;
+	public static ArrayList<Individual> populations;
+	public static ArrayList<ArrayList<Individual>> pathList;
 
-	public static int population = 10;
-	public static double crossoverRate = 0.85;
-	public static double mutationRate = 0.05;
-	public static int maxGeneration = 500;
+	public static int maxPopulation = 10;
+	public static double crossoverRate = 0.70;
+	public static double mutationRate = 0.10;
+	public static int maxGeneration = 100;
 
+	/**
+	 * Initialization
+	 */
 	public static void initialize() {
-		populations = new HashSet<Individual>();
+		populations = new ArrayList<Individual>();
+		pathList = new ArrayList<ArrayList<Individual>>();
 
-		for (int i = 0; i < population; i++) {
+		for (int i = 0; i < maxPopulation; i++) {
 			populations.add(new Individual());
 		}
-
 	} // end of initialize()
 
-	public static HashSet<Individual> crossover(Individual f, Individual m) {
+	public static ArrayList<Individual> selection() {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Make two parent individual crossover and generate children
+	 * 
+	 * @param f
+	 *            - parent
+	 * @param m
+	 *            - parent
+	 * @return
+	 */
+	public static ArrayList<Individual> crossover(Individual f, Individual m) {
 		int cutpoint = (int) (Math.random() * (2 * GA_HW1.numberOfBits - 1) + 1);
 
-		String child_1 = f.getGene().substring(0, cutpoint) + " " + m.getGene().substring(cutpoint, m.getGene().length());
-		String child_2 = m.getGene().substring(0, cutpoint) + " " + f.getGene().substring(cutpoint, f.getGene().length());
+		String child_1 = f.getGene().substring(0, cutpoint) + m.getGene().substring(cutpoint, m.getGene().length());
+		String child_2 = m.getGene().substring(0, cutpoint) + f.getGene().substring(cutpoint, f.getGene().length());
 
-		HashSet<Individual> childs = new HashSet<Individual>();
-		childs.add(new Individual(child_1));
-		childs.add(new Individual(child_2));
+		ArrayList<Individual> child = new ArrayList<Individual>();
+		child.add(new Individual(child_1));
+		child.add(new Individual(child_2));
 
-		return childs;
+		return child;
 	} // end of crossover()
 
+	/**
+	 * Randomly mutate gene at random position
+	 * 
+	 * @param input
+	 *            - Individual
+	 * @return mutated Individual
+	 */
 	public static Individual mutate(Individual input) {
 		String gene = input.getGene();
 		gene = GA_HW1.reverseBit(gene, (int) (Math.random() * gene.length()));
@@ -115,27 +137,63 @@ public class ClassicalGA {
 		return new Individual(gene);
 	}// end of mutate()
 
-	public static void main(String[] args) {
-		Individual a = new Individual("00000000001111111111");
-		System.out.println(a.getGene());
+	/**
+	 * Sort populations and keep top N individuals
+	 */
+	public static void keepBest() {
+		// Sort populations by decreasing order
+		populations.sort(new Comparator<Individual>() {
+			public int compare(Individual o1, Individual o2) {
+				return Double.compare(o2.getFitness(), o1.getFitness());
+			}
+		});
 
+		// Keep top N individuals
+		while (populations.size() > maxPopulation) {
+			populations.remove(populations.size() - 1);
+		}
+	}// end of keepBest()
+
+	/**
+	 * Operation taken for each generation
+	 */
+	public static void nextGeneration() {
+		// Crossover
+		int index1;
+		int index2;
+		while (Math.random() < crossoverRate) {
+			index1 = (int) (Math.random() * populations.size());
+			index2 = (int) (Math.random() * populations.size());
+			populations.addAll(crossover(populations.get(index1), populations.get(index2)));
+		}
+
+		// Mutation
+		int index;
+		while (Math.random() < mutationRate) {
+			index = (int) (Math.random() * populations.size());
+			populations.set(index, mutate(populations.get(index)));
+		}
+
+		keepBest();
+
+		pathList.add(new ArrayList<Individual>(populations));
+	}// end of nextGeneration()
+
+	public static void main(String[] args) {
 		initialize();
 
-		System.out.println("=====");
-		for (int i = 0; i < 100; i++) {
-			Individual b = new Individual();
-			Individual c = new Individual();
-			System.out.println(b.getGene());
-			System.out.println(c.getGene());
-			crossover(b, c);
-			System.out.println();
+		for (int i = 0; i < maxGeneration; i++) {
+			nextGeneration();
 		}
-		// Individual b = new Individual();
-		// Individual c = new Individual();
-		// System.out.println(b.getGene());
-		// System.out.println(c.getGene());
-		// crossover(b, c);
+
+		System.out.println("=====");
+
+		for (ArrayList<Individual> arrayList : pathList) {
+			for (Individual individual : arrayList) {
+				System.out.println(individual.getX() + " " + individual.getY() + " " + individual.getFitness());
+			}
+			System.out.println("*****");
+		}
 
 	} // end of main()
-
 } // end of class ClassicalGA
